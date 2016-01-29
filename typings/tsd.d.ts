6639013fe3d17e270a7ed67f79f0d7c7d393869d
bouzuya <m@bouzuya.net>
2016-01-29 13:26:48 +0900

    add vdom-parser as dependency

---
+++ typings/tsd.d.ts
@@ -0,0 +1,11 @@
+/// <reference path="empower/empower.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
+/// <reference path="power-assert-formatter/power-assert-formatter.d.ts" />
+/// <reference path="power-assert/power-assert.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="virtual-dom/virtual-dom.d.ts" />
+
+declare module 'vdom-parser' {
+  var parse: any;
+  export default parse;
+}
